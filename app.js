0150272eb1336ee654f3823ab758bdf214088c8f
CasuallyDreamin <ysn.arambash@gmail.com>
2025-08-02 07:05:12 -0400

    implemented draw on hover and centered the container

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const canvas_dimension = 960;
 
 function gridify(dimension) {
     for(let i = 0; i < dimension; i++) {
@@ -12,19 +13,32 @@ function gridify(dimension) {
         
         container.appendChild(new_row);
     }
+    return true
 }
 
 function createSquare() {
     const new_square = document.createElement("div");
+
     new_square.setAttribute("class", "square");
+    new_square.addEventListener("mouseover", () => {
+       new_square.style.backgroundColor = "black";
+    })  
     return new_square;
 }
 
 function changeDimensions() {
     let new_dimension = prompt("Enter new grid dimension");
     new_dimension = parseInt(new_dimension);
+    if (typeof(new_dimension) !== 'number') {
+        alert("Invalid input. must be a number");
+        return false
+    } 
+    if (new_dimension < 0 || new_dimension > 100) {
+        alert("please enter a number between 0 and 100");
+        return false
+    }
     container.textContent = '';
-    gridify(new_dimension);
+    return gridify(new_dimension);
 }
 
 gridify(16)
